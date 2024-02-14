fcd7f98ea3d8bcdfcaa6dec0187e273b617e407c
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2024-02-14 14:11:14 -0300

    Add dog-view and some other codes

--- standalone/walk-my-dog/my-code/walk-my-dog/src/app/app.component.ts
+++ standalone/walk-my-dog/my-code/walk-my-dog/src/app/app.component.ts
@@ -1,44 +1,42 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <nav>
-      <div>
-        <img class="logo" src="assets/dog-walker-logo.svg">
-        <h1 class="title">Walk My Dog</h1>
-      </div>
-    </nav>
-    <router-outlet></router-outlet>
-  `,
-  styles: [
-    `
-  nav {
-      display: flex;
-      justify-content: space-between;
-      margin: 20px 10px 10px 10px;
-    }
-    
-    nav div.menu-items {
-      display: flex;
-      width: 10%;
-      justify-content: space-between;
-    }
-    nav > div {
-      display: flex;
-    }
-    .title {
-      font-size: 25pt;
-    }
-    .logo {
-      height: 40px;
-    }
-  `
-  ],
-})
-export class AppComponent {
-  title = 'walk-my-dog';
-}
+ @Component({
+   selector: 'app-root',
+   template: `
+     <nav>
+       <div>
+         <a><img class="logo" src="assets/dog-walker-logo.svg"></a>
+         <h1 class="title">Walk My Dog</h1>
+       </div>
+     </nav>
+     <router-outlet></router-outlet>
+   `,
+   styles: [
+     `
+   nav {
+       display: flex;
+       justify-content: space-between;
+       margin: 20px 10px 10px 10px;
+     }
+     
+     nav div.menu-items {
+       display: flex;
+       width: 10%;
+       justify-content: space-between;
+     }
+     nav > div {
+       display: flex;
+     }
+     .title {
+       font-size: 25pt;
+     }
+     .logo {
+       height: 40px;
+     }
+   `
+   ]
+ })
+ export class AppComponent {
+   title = 'walk-my-dog';
+ }
+ 
--- standalone/walk-my-dog/my-code/walk-my-dog/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { DogsListComponent } from './dogs-list.component';
-
-export const routes: Routes = [
-    { path: '', pathMatch: 'full', redirectTo: 'list' },
-    { path: 'list', component: DogsListComponent },
-];

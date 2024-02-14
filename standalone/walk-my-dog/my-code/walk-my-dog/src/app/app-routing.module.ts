70ed9a17e7676493303676d6f061e9ebb44b99bd
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2024-02-14 14:16:04 -0300

    Use lazy loading

--- standalone/walk-my-dog/my-code/walk-my-dog/src/app/app-routing.module.ts
+++ standalone/walk-my-dog/my-code/walk-my-dog/src/app/app-routing.module.ts
@@ -14,8 +14,8 @@ import { DogsListComponent } from './dogs-list.component';
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'list' },
   { path: 'list', component: DogsListComponent },
-  { path: 'details/:index', component: DogViewComponent }
-  //{ path: 'details/:index', loadComponent: () => import('./dog-view.component').then(m => m.DogViewComponent) }
+  //{ path: 'details/:index', component: DogViewComponent }
+  { path: 'details/:index', loadComponent: () => import('./dog-view.component').then(m => m.DogViewComponent) }
 ];
 
 @NgModule({
